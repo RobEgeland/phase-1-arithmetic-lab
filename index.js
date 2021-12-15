75851ea299776d83dc3680b272d7f90a99563fb7
Robert Egeland <robegeland@yahoo.com>
2021-12-14 22:00:36 -0600

    commit tree 1109d72c4c8f314ac3e5777e326ef8acdcc1478d

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1 = 2;
 
-// Write your code here
+let num2 = 31;
+
+let num3 = 12;
+
+let num4 = 3;
+
+let multiply = num1 * num2; 
+
+let random = Math.ceil(Math.random());
+
+let mod = num3 / num4;
+
+let max = Math.max(4, 2, 12, 15, 1, 20);
